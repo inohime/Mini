f6af4c15acfe564f50f205cea972363b83a0d5cd
Eris <29220293+inohime@users.noreply.github.com>
2023-05-25 10:25:15 -0700

    formatted tag seeker

--- tag_seeker.sh
+++ tag_seeker.sh
@@ -1,1 +1,8 @@
-for i in {1..500}; do curl -g "https://danbooru.donmai.us/tags.json?login={LOGIN}&api_key={API_KEY}&search[post_count.gte]=8000&search[order]=count&limit=5&page=$i" >> "v.json"; if (( $i % 30 == 0 )); then sleep 60; fi; done; jq -s 'flatten' v.json > v_tmp.json && mv v_tmp.json v.json
+for i in {1..500}
+    do curl -g "https://danbooru.donmai.us/tags.json?login={LOGIN}&api_key={API_KEY}&search[post_count.gte]=8000&search[order]=count&limit=5&page=$i" >> "v.json";
+    if (( $i % 30 == 0 )); 
+        then sleep 60; 
+    fi
+done
+
+jq -s 'flatten' v.json > v_tmp.json && mv v_tmp.json v.json
